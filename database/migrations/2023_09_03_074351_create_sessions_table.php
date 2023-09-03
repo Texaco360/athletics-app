91a402a9ee060309709fc387751e5b4fc8b4c612
Koenraad <koenraad@libaro.be>
2023-09-03 10:22:52 +0200

    first database migrations

---
+++ database/migrations/2023_09_03_074351_create_sessions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->foreigId('location_id');
+            $table->unsignedTinyInteger('type')->default(0);
+            $table->string('notes')->nullable();
+            $table->date('start');
+            $table->date('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainings');
+    }
+};
